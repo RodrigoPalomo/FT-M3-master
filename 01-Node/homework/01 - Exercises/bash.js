537cca72c6d3f2b2f94c687cd2b275aa05b553bb
Rodrigo Palomo <repmlm@hotmail.com>
2023-02-23 13:24:58 -0300

    homework 01-Node (complete)

--- 01-Node/homework/01 - Exercises/bash.js
+++ 01-Node/homework/01 - Exercises/bash.js
@@ -1,19 +1,27 @@
 const process = require("process");
 const commands = require("./commands/index.js");
 
-function bash() {
-  process.stdout.write("prompt >")
-  process.stdin.on("data", (data)=> {
-    let args = data.toString().trim().split('');
-    let cmd = args.shift();
-  })
-}
-
-function print(output){
+const print = (output) => {
   process.stdout.write(output);
-  process.stdout.write("\nprompt >");
-}
+  process.stdout.write("\nprompt > ");
+};
+const bash = () => {
+  process.stdout.write("prompt > ");
 
+  process.stdin.on("data", (data) => {
+    let args = data.toString().trim().split(' ');
+      // Después de mucho tiempo comparando con otro código
+      // Me dí cuenta que el .split() tiene que ir con espacio split(' ') <===
+      // Sino no anda, no sé por qué
+    let cmd = args.shift();
+
+    commands[cmd]
+      ? commands[cmd](print, args)
+      : print(`command not found: ${cmd}`);
+  });
+};
+
+bash();
 
 module.exports = {
   print,
--- 01-Node/homework/01 - Exercises/commands/index.js
+++ 01-Node/homework/01 - Exercises/commands/index.js
@@ -2,31 +2,58 @@ const fs = require("fs");
 const utils = require("../utils/request");
 const process = require("process");
 
-function pwd(a) {
-    return a
-}
+const pwd = (print, _args) => {
+  print(process.cwd());
+};
 
-function date() {}
+const date = (print, _args) => {
+  print(Date());
+};
 
-function echo() {}
+const echo = (print, args) => {
+  print(args.join(" "));
+};
 
-function ls() {}
+const ls = (print, args) => {
+  fs.readdir(".", "utf-8", (err, files) => {
+    if (err) throw Error(err);
+    print(files.join("\n"));
+  });
+};
 
-function cat() {}
+const cat = (print, args) => {
+  fs.readFile(args.join(""), "utf-8", (err, data) => {
+    if (err) throw Error(err);
+    print(data);
+  });
+};
 
-function head() {}
+const head = (print, args) => {
+  fs.readFile(args.join(""), "utf-8", (err, data) => {
+    if (err) throw Error(err);
+    print(data.split("\n").splice(0, 1).join(""));
+  });
+};
 
-function tail() {}
+const tail = (print, args) => {
+  if (err) throw Error(err);
+  print(data.split("\n").at(-1));
+};
 
-function curl() {}
+const curl = (print, args) => {
+  utils.request(args.join(""), (err, response) => {
+    if (err) throw Error(err);
+    print(response.data)
+  });
+};
 
 module.exports = {
-    pwd,
-    date,
-    echo,
-    ls,
-    cat,
-    head,
-    tail,
-    curl
+  pwd,
+  date,
+  echo,
+  ls,
+  cat,
+  head,
+  tail,
+  curl,
 };
