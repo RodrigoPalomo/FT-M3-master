9eb0addae5fb60137c6f001b84ae265313ec059b
Rodrigo Palomo <repmlm@hotmail.com>
2023-03-01 10:35:03 -0300

    homework 05

--- 05-Express/homework/01 - Exercises/src/server.js
+++ 05-Express/homework/01 - Exercises/src/server.js
@@ -1,10 +1,79 @@
 const express = require("express");
-
 let publications = [];
-
 const server = express();
-
 server.use(express.json());
+
+let id = 1;
+
+server.post("/posts", (req, res) => {
+  const { author, title, contents } = req.body;
+  if (author && title && contents) {
+    const publication = {
+      author,
+      title,
+      contents,
+      id: id++,
+    };
+    publications.push(publication);
+
+    res.status(200).json(publication);
+  } else {
+    return res.status(400).json({
+      error:
+        "No se recibieron los parámetros necesarios para crear la publicación",
+    });
+  }
+});
+
+server.get("/posts", (req, res) => {
+  const { author, title } = req.query;
+
+  if (author && title) {
+    const publicationFiltered = publications.filter(
+      (publi) => publi.author === author && publi.title === title
+    );
+    publicationFiltered.length
+      ? res.status(200).json(publicationFiltered)
+      : res.status(400).json({
+          error:
+            "No existe ninguna publicación con dicho título y autor indicado",
+        });
+  }
+});
+
+server.get("/posts/:author", (req, res) => {
+  const { author } = req.params;
+  const publicationFiltered = publications.filter(
+    (publi) => publi.author === author
+  );
+  if (publicationFiltered.length) {
+    return res.status(200).json(publicationFiltered);
+  } else {
+    res
+      .status(400)
+      .json({ error: "No existe ninguna publicación del autor indicado" });
+  }
+});
+
+server.put("/posts/:id", (req, res) => {
+  const { id } = req.params;
+  const { title, contents } = req.body;
+
+  if (title && contents && id) {
+    const publicationId = publications.find((publi) => publi.id === Number(id));
+    !publicationId
+    ? res.status(400).json(publicationId)
+    : (
+        publicationId = {... publicationId, title, contents}
+        && res.status(200).json(publicationId)
+    )
+  } else {
+    res.status(400).json({
+      error:
+        "No se recibieron los parámetros necesarios para modificar la publicación",
+    });
+  }
+});
 
 //NO MODIFICAR EL CODIGO DE ABAJO. SE USA PARA EXPORTAR EL SERVIDOR Y CORRER LOS TESTS
 module.exports = { publications, server };
